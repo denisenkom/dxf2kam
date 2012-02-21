d39d4f2628a4c51be38fca33f3cc3573dc01076d
Mikhail Denisenko <denisenkom@gmail.com>
2012-02-21 00:02:30 -0500

    merged with dxf2kam2

--- dxf.cpp
+++ dxf.cpp
@@ -2,6 +2,9 @@
 //
 #include <stdexcept>
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <stack>
 #include <cassert>
 #include <cstdlib>
 #include "dxf.h"
@@ -464,4 +467,498 @@ void dxf::parse(istream &reader, Factory &factory)
 {
 	Parser parser(reader, factory);
 	parser.parse();
+}
+
+namespace _newloader
+{
+
+
+enum Manipulator {
+	Eof,
+	EndSection,
+	EndTable,
+	EndBlock,
+	BeginNode
+};
+
+class ParseError
+{
+public:
+	const char *GetMessageA();
+};
+
+typedef std::map<std::string, class Node*> Nodes;
+typedef std::map<unsigned, class Attribute> attributes_t;
+
+class Node
+{
+public:
+	Nodes Children;
+	attributes_t Attributes;
+};
+
+class Attribute : public Node
+{
+public:
+	enum Type {String, Short, Long, Double, Boolean, Vector};
+
+	const char *GetName();
+
+	//explicit operator (const char *());
+	operator int();
+	operator float();
+	operator float*();
+
+	const char *GetValue();
+
+	Type        GetType() const {return _type;};
+
+	unsigned GetCode() const {return _code;};
+
+	Attribute(unsigned c, std::string value);
+	Attribute(int rem, std::string value[3]);
+
+private:
+	unsigned _code;
+	Type _type;
+	std::string _sVal[3];
+	union {
+		long _lVal;
+		short _shVal;
+		double _dVal;
+		bool _bVal;
+		double _vecVal[3];
+	};
+};
+
+class Constructor
+{
+	std::string _xyz[10][3];
+	std::stack<Node*> _stack;
+
+public:
+	Constructor& operator << (Manipulator);
+	Constructor& operator << (const Attribute &);
+};
+
+class Parser
+{
+public:
+	static void Parse(std::istream &, Constructor &);
+};
+
+class Entity : public Node
+{
+public:
+	Entity(Node &source);
+
+	Attribute& Type();          // 0 Entity type
+
+	Attribute& Handle();        // 5 Handle
+
+	Attribute& LayerName();     // 8 Layer name
+
+	Attribute& LinetypeName();  // 6 Linetype name (present if not BYLAYER). The special name BYBLOCK indicates a floating linetype (optional)
+
+	// <summary>
+	// Code: 62
+	// Color number (present if not BYLAYER); zero indicates the BYBLOCK (floating) color;
+	// 256 indicates BYLAYER; a negative value indicates that the layer is turned off (optional) 
+	// </summary>
+	Attribute& ColorNumber(); 
+
+	Attribute& Lineweight();    // 370 Lineweight enum value. Stored and moved around as a 16-bit integer.
+
+	Attribute& LinetypeScale(); // 48 Linetype scale (optional) (1.0)
+
+	Attribute& Visibility();    // 60 Object visibility (optional): 0 = Visible; 1 = Invisible (0)
+};
+
+typedef std::vector<Entity*> Entities;
+typedef Entities::iterator EntitiesIt;
+
+class Line : public Entity
+{
+public:
+	Attribute& Thickness();  // 39 Thickness (optional; default = 0)
+
+	Attribute& StartPoint(); // 10 Start point (in WCS)
+
+	Attribute& EndPoint();   // 11 Endpoint (in WCS)
+};
+
+class Point : public Entity
+{
+public:
+	Attribute& Location();   // 10 Point location (in WCS)
+
+	Attribute& Thickness();  // 39 Thickness (optional; default = 0)
+};
+
+class Circle : public Entity
+{
+public:
+	Attribute& Thickness();  // 39 Thickness (optional; default = 0)
+
+	Attribute& Center();     // 10 Center point (in OCS)
+
+	Attribute& Radius();     // 40 Radius
+};
+
+class Arc : public Circle
+{
+public:
+	Attribute& StartAngle(); // 50 Start angle
+
+	Attribute& EndAngle();   // 51 End angle
+};
+
+class Ellipse : public Entity
+{
+public:
+	Attribute& Center();   // 10 Center point (in WCS)
+
+	Attribute& MajorEnd(); // 11 Endpoint of major axis, relative to the center (in WCS)
+
+	Attribute& Ratio();    // 40 Ratio of minor axis to major axis
+
+	Attribute& Start();    // 41 Start parameter (this value is 0.0 for a full ellipse)
+
+	Attribute& End();      // 42 End parameter (this value is 2pi for a full ellipse)
+};
+
+class Database : public Node
+{
+	std::string _xyz[10][3];
+	std::stack<Node*> _stack;
+    
+public:
+	Nodes Sections;
+
+	Nodes::iterator FindSection(const char *name);
+
+	Entities GetEntities() const;
+
+    void Load(const char *fileName);
+
+	void Save(const char *fileName);
+};
+
+const short MAXSHORT = short(0x7fff);
+const short MINSHORT = short(0x8000);
+const int MAX_LINE_LENGTH = 1024;
+
+void Database::Load(const char *fileName)
+{
+	_stack.push(this);
+	ifstream fs(fileName);
+	if (!fs)
+		throw ParseError(); // unable to open file
+
+	while (fs)
+	{
+		char buf[MAX_LINE_LENGTH];
+		fs.getline(buf, sizeof(buf));
+		int code = atoi(buf);
+
+		fs.getline(buf, sizeof(buf));
+		string str(buf);
+
+		if (code == 0)
+		{
+			if (str == "EOF")
+				return;
+			else if (str == "ENDSEC")
+			{
+				while (_stack.size() > 1)
+				{
+					Node *b = _stack.top();
+					_stack.pop();
+					_stack.top()->Children.insert(Nodes::value_type(
+						b->Attributes.begin()->second.GetValue(), b));
+				}
+			}
+			else if (str == "ENDTAB" || str == "ENDBLK")
+			{
+				Node *b = _stack.top();
+				_stack.pop();
+				_stack.top()->Children.insert(Nodes::value_type(
+					b->Attributes.begin()->second.GetValue(), b));
+			}
+			else // new node
+			{
+				_stack.push(new Node());
+				_stack.top()->Attributes.insert(attributes_t::value_type(code,
+					Attribute(0, str)));
+			}
+		}
+		else
+		{
+			if (code >= 10 && code <= 39)
+			{
+				// vector param
+				int loDigit = code % 10;
+				int hiDigit = code / 10;
+				_xyz[loDigit][hiDigit] = str;
+				if (hiDigit == 3)
+					_stack.top()->Attributes.insert(attributes_t::value_type(loDigit,
+						Attribute(loDigit, _xyz[loDigit])));
+			}
+			else
+				// scalar param
+				_stack.top()->Attributes.insert(
+					attributes_t::value_type(code, Attribute(code, str)));
+		}
+	}
+}
+
+Attribute::Attribute(unsigned c, std::string value)
+{
+	_sVal[0] = value;
+	_code = c;
+
+	// decoding type
+
+	if (c <= 9) // 0-9
+		_type = String; // String (with the introduction of extended symbol names in AutoCAD 2000, the 255-character limit has been lifted. There is no explicit limit to the number of bytes per line, although most lines should fall within 2049 bytes)
+	else if (c <= 39) // 10-39
+		assert(0); // this case should be processed in procedure below
+	else if (c <= 59) // 40-59
+		_type = Double; // Double-precision floating-point value
+	else if (c <= 79) // 60-79
+		_type = Short; // 16-bit integer value
+	else if (c <= 89) // 80-89
+		throw ParseError(); // invalid code
+	else if (c <= 99) // 90-79
+		_type = Long; // 32-bit integer value
+	else if (c == 100)
+		_type = String; // String (255-character maximum; less for Unicode strings)
+	else if (c == 101)
+		throw ParseError(); // invalid code
+	else if (c == 102)
+		_type = String; // String (255-character maximum; less for Unicode strings)
+	else if (c <= 104) // 103-104
+		throw ParseError(); // invalid code
+	else if (c == 105)
+		_type = String; // String representing hexadecimal (hex) handle value
+	else if (c <= 109) // 106-109
+		throw ParseError(); // invalid code
+	else if (c <= 149) // 110-119 120-129 130-139 140-149
+		_type = Double;
+	else if (c <= 169) // 150-169
+		throw ParseError(); // invalid code
+	else if (c <= 179) // 170-179
+		_type = Short;
+	else if (c <= 209) // 180-209
+		throw ParseError(); // invalid code
+	else if (c <= 239) // 210-239
+		_type = Double;
+	else if (c <= 269) // 240-269
+		throw ParseError(); // invalid code
+	else if (c <= 289) // 270-279 280-289
+		_type = Short;
+	else if (c <= 299) // 290-299
+		_type = Boolean; // Boolean flag value
+	else if (c <= 309) // 300-309
+		_type = String; // Arbitrary text string
+	else if (c <= 319) // 310-319
+		_type = String; // String representing hex value of binary chunk
+	else if (c <= 329) // 320-329
+		_type = String; // String representing hex handle value
+	else if (c <= 369) // 330-369
+		_type = String; // String representing hex object IDs
+	else if (c <= 389) // 370-379 380-389
+		_type = Short;
+	else if (c <= 399) // 390-399
+		_type = String; // String representing hex handle value
+	else if (c <= 409) // 400-409
+		_type = Short;
+	else if (c <= 419) // 410-419
+		_type = String; // String
+	else if (c <= 429) // 420-429
+		_type = Long;
+	else if (c <= 439) // 430-439
+		_type = String;
+	else if (c <= 449) // 440-449
+		_type = Long;
+	else if (c <= 459) // 450-459
+		_type = Long; // Long?
+	else if (c <= 469) // 460-469
+		_type = Double;
+	else if (c <= 479) // 470-479
+		_type = String;
+	else if (c <= 998) // 480-998
+		throw ParseError(); // invalid code
+	else if (c == 999) // 999
+		_type = String; // Comment (string)
+	else if (c <= 1009) // 1000-1009
+		_type = String; // String (same limits as indicated with 0–9 code range)
+	else if (c <= 1059) // 1010-1059
+		_type = Double;
+	else if (c <= 1070)	// 1060-1070
+		_type = Short;
+	else if (c == 1071) // 1071
+		_type = Long;
+	else                // 1072...
+		throw ParseError(); // invalid code
+	
+	// parsing integer and floating point values
+
+	char *end;
+	switch (_type)
+	{
+	case String:
+		break; // do nothing
+
+	case Short:
+	case Long:
+	case Boolean:
+		long res;
+		res = strtol(_sVal[0].c_str(), &end, 10);
+		if (errno == ERANGE)
+			throw ParseError(); // overflow converting integer
+		switch (_type)
+		{
+		case Short:
+			assert(MINSHORT <= res && res <= MAXSHORT);
+			_shVal = short(res);
+			break;
+		case Long:
+			_lVal = res;
+			break;
+		case Boolean:
+			// TODO: check value
+			assert(res == 0 || res == 1);
+			_bVal = res == 1 ? true : false;
+			break;
+		default:
+			assert(0); // should never get here
+			break;
+		}
+		break;
+
+	case Double:
+		_dVal = strtod(_sVal[0].c_str(), &end);
+		if (errno == ERANGE)
+			throw ParseError(); // overflow converting double
+		break;
+
+	default:
+		assert(0); // should never get here
+		break;
+	}
+}
+
+Attribute::Attribute(int rem, string value[3])
+{
+	assert(0 <= rem && rem <= 9);
+
+	_code = 10 + rem;
+	_type = Vector;
+
+	// converting value
+
+	double *v = _vecVal;
+	char *end;
+	for (string* s = value; s != value + sizeof(value)/sizeof(*value); s++, v++)
+	{
+		*v = strtod(s->c_str(), &end);
+		if (errno == ERANGE)
+			throw ParseError(); // overflow converting 3D vector values
+	}
+}
+
+
+const char * Attribute::GetValue()
+{
+	return _sVal[0].c_str();
+}
+
+Constructor& Constructor::operator <<(Manipulator manip)
+{
+	Node *node;
+	switch (manip)
+	{
+	case Eof:
+		break;
+	case EndSection:
+		while (_stack.size() > 1)
+		{
+			node = _stack.top();
+			_stack.pop();
+			_stack.top()->Children.insert(make_pair(node->Attributes.begin()->second.GetValue(), node));
+		}
+		break;
+	case EndBlock:
+	case EndTable:
+		node = _stack.top();
+		_stack.pop();
+		_stack.top()->Children.insert(make_pair(node->Attributes.begin()->second.GetValue(), node));
+		break;
+	case BeginNode:
+		_stack.push(new Node());
+		break;
+	default:
+		assert(0/*invalid manip*/);
+		break;
+	}
+	return *this;
+}
+
+Constructor& Constructor::operator <<(const Attribute &attr)
+{
+	_stack.top()->Attributes.insert(attributes_t::value_type(attr.GetCode(), attr));
+	return *this;
+}
+
+void Parser::Parse(istream &stream, Constructor &constructor)
+{
+	char buf[MAX_LINE_LENGTH];
+	string xyz[3];
+	while (stream)
+	{
+		// Getting code
+		//
+		stream.getline(buf, sizeof(buf));
+		unsigned code = atoi(buf);
+
+		// Getting string
+		//
+		stream.getline(buf, sizeof(buf));
+		string str = buf;
+
+		if (code == 0) // 0 code for reserved words and nodes 
+		{
+			if (str == "EOF")
+			{
+				constructor << Eof;
+				break;
+			}
+			else if (str == "ENDSEC")
+				constructor << EndSection;
+			else if (str == "ENDTAB")
+				constructor << EndTable;
+			else if (str == "ENDBLK")
+				constructor << EndBlock;
+			else // new node like SECTION ENTITY TABLE BLOCK etc.
+			{
+				constructor << BeginNode;
+				constructor << Attribute(code, str);
+			}
+		}
+		else // other codes is attributes
+		{
+			if (code >= 10 && code <= 39) // 3d vector attribute
+			{
+				int rem = code % 10;
+				int fix = code / 10;
+				xyz[fix] = str;
+				if (fix == 3) // got last 3rd value
+					constructor << Attribute(rem, xyz[rem]);
+			}
+			else // scalar attribute	
+				constructor << Attribute(code, str);
+		}
+	}
+}
 }
